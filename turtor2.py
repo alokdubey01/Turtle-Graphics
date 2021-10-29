10baeee6f189df6d419e9dd61e55c7b42650527b
alokdubey01 <alok73327@gmail.com>
2021-10-29 20:36:08 +0530

    uploaded

---
+++ turtor2.py
@@ -0,0 +1,8 @@
+import turtle
+window = turtle.Screen()
+pen = turtle.Turtle()
+pen.fillcolor("red")
+pen.begin_fill()
+pen.circle(20)
+pen.end_fill()
+turtle.done()
---
+++ tutor.py
@@ -0,0 +1,26 @@
+import turtle
+w = turtle.Screen()
+# to set background color of screen
+w.bgcolor("black")
+# to set the output title
+turtle.title("hey")
+# to create a variable with turtor module
+mpen = turtle.Turtle()
+# To set the heading of cursor
+# mpen.setheading(180)
+# # pen color
+mpen.color("white")
+#
+#
+# # for using loop
+# for i in range(6):
+#     mpen.forward(100)
+#     mpen.right(90)
+#     mpen.left(30)
+#
+# # for hide arrow from the screen
+# mpen.hideturtle()
+#help(turtle.circle)
+# mpen.circle(100,180)
+# mpen.circle(100,steps=3)     #shanku
+turtle.done()
